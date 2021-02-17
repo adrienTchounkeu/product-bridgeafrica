8d090ea10f9ac59f7d3cb01f4f3626c64545f1a4
Adrien Tchounkeu <adrientchounkeu10@gmail.com>
2021-02-17 17:01:16 +0100

    - create the Product component - add the store

---
+++ src/components/Product.vue
@@ -0,0 +1,21 @@
+<template>
+	
+</template>
+
+<script>
+    export default {
+        name: "Product",
+        data() {
+            return {}
+        },
+        created() {},
+        mounted() {},
+        watch: {},
+        computed: {},
+        methods: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,52 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        //the idCount gives the id of each product, it is a manually increment property
+        idCount: 0,
+        user: {},
+        products: []
+    },
+    mutations: {
+        // add a product
+        addProduct(state, product) {
+
+            // add the product to the product List
+            state.products.push({
+                id: state.idCount,
+                ...product
+            })
+            // increment the idCount
+            state.idCount++
+        },
+
+        // update a product
+        updateProduct(state, product){
+            const intermediateProductList = state.products.filter(prod => prod.id != product.id)
+            state.products = intermediateProductList.concat([
+                product
+            ])
+        },
+
+        // delete a product
+
+        deleteProduct(state, idProduct){
+            state.products = state.products.filter(prod => prod.id != idProduct)
+        }
+
+    },
+    getters: {
+
+        // display the product list
+        productsList: state => {
+            return state.products
+        },
+
+        // display a product information
+        productInfo: (state) => (id) => {
+            return state.products.find(prod => prod.id == id)
+        }
+    },
+    actions: {},
+    modules: {}
 })
